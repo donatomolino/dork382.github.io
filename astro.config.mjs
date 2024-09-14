9f28ec375be520c3e534c3e334ca586ea231b752
Donato Molino <dork3802@gmail.com>
2024-09-14 17:00:41 +0200

    remove base site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://donatomolino.it",
-  base: "dork382.github.io",
   integrations: [tailwind(), icon()],
 });
